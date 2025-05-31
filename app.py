8f029fbb105b6d9697ba99312e99b0c209c5c5b8
Vatsal Gaur <70809307+vatsal1704@users.noreply.github.com>
2025-05-31 19:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,26 +20,26 @@ def prediction(input_list):
     else:
         return f'This booking is less likely to get canceled, chances {round(pred,2)}'
     
-def main():
-    st.title('INN HOTEL GROUP')
-    lt = st.text_input('Enter the lead time in days')
-    mkt = (lambda x:1 if x=='Online' else 0)(st.selectbox('How the booking was made',['Online','Offline']))
-    price = st.text_input('Enter the price of the room')
-    adult = st.selectbox('How many adults',[1,2,3,4])
-    arr_m = st.slider('What is the month of arrival?',min_value=1,max_value=12,step=1)
-    weekd_lambda= (lambda x: 0 if x=='Mon' else
-                             1 if x=='Tue' else
-                             2 if x=='Wed' else
-                             3 if x=='Thus' else
-                             4 if x=='Fri' else
-                             5 if x=='Sat' else 6)
-    arr_w = weekd_lambda(st.selectbox('What is the weekday of arrival',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
-    dep_w = weekd_lambda(st.selectbox('What is the weekday of departure',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
-    weekn = st.text_input('Enter the no of week nights in stay')
-    wkndn = st.text_input('Enter the no of weekend nights in stay')
-    totan = weekn + wkndn
-    park = (lambda x:1 if x=='yes' else 0)(st.selectbox('Does customer need parking',['yes','no']))
-    spcl = st.selectbox('How many special requests have been mode',[0,1,2,3,4,5])
+# def main():
+#     st.title('INN HOTEL GROUP')
+#     lt = st.text_input('Enter the lead time in days')
+#     mkt = (lambda x:1 if x=='Online' else 0)(st.selectbox('How the booking was made',['Online','Offline']))
+#     price = st.text_input('Enter the price of the room')
+#     adult = st.selectbox('How many adults',[1,2,3,4])
+#     arr_m = st.slider('What is the month of arrival?',min_value=1,max_value=12,step=1)
+#     weekd_lambda= (lambda x: 0 if x=='Mon' else
+#                              1 if x=='Tue' else
+#                              2 if x=='Wed' else
+#                              3 if x=='Thus' else
+#                              4 if x=='Fri' else
+#                              5 if x=='Sat' else 6)
+#     arr_w = weekd_lambda(st.selectbox('What is the weekday of arrival',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
+#     dep_w = weekd_lambda(st.selectbox('What is the weekday of departure',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
+#     weekn = st.text_input('Enter the no of week nights in stay')
+#     wkndn = st.text_input('Enter the no of weekend nights in stay')
+#     totan = weekn + wkndn
+#     park = (lambda x:1 if x=='yes' else 0)(st.selectbox('Does customer need parking',['yes','no']))
+#     spcl = st.selectbox('How many special requests have been mode',[0,1,2,3,4,5])
     
     # lt_t,price_t = transformer.transform([[lt,price]])[0]
     
